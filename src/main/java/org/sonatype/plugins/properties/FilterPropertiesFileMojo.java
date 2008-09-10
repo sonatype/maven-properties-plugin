47b1d7588feb5db496057c35539ce441abe4e979
Igor Fedorenko <ifedorenko@sonatype.com>
2008-09-10 19:06:09 +0000

    Share project "maven-properties-plugin" into "https://svn.sonatype.org/plugins"
    git-svn-id: file:///opt/svn/repositories/sonatype.org/plugins/trunk/maven-properties-plugin@84 c2a14038-686d-4adc-8740-a14e5738cb64

---
+++ src/main/java/org/sonatype/plugins/properties/FilterPropertiesFileMojo.java
@@ -0,0 +1,98 @@
+package org.sonatype.plugins.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.filtering.MavenProjectValueSource;
+import org.codehaus.plexus.interpolation.InterpolationException;
+import org.codehaus.plexus.interpolation.StringSearchInterpolator;
+import org.codehaus.plexus.interpolation.ValueSource;
+
+/**
+ * @goal filter-file
+ */
+public class FilterPropertiesFileMojo extends AbstractMojo {
+
+	/**
+	 * @parameter expression="${project}"
+	 */
+	private MavenProject mavenProject;
+	
+	/**
+	 * @parameter expression="${session}"
+	 */
+	private MavenSession mavenSession;
+	
+	/**
+	 * @parameter
+	 * @read-only
+	 * @required
+	 */
+	private File in;
+	
+	/**
+	 * @parameter
+	 * @read-only
+	 * @required
+	 */
+	private File outDir;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		final Properties baseProps = new Properties();
+		if (mavenProject.getProperties() != null) {
+			baseProps.putAll(mavenProject.getProperties());
+		}
+		if (mavenSession.getExecutionProperties() != null) {
+			baseProps.putAll(mavenSession.getExecutionProperties());
+		}
+		
+        StringSearchInterpolator interpolator = new StringSearchInterpolator();
+        interpolator.addValueSource(new MavenProjectValueSource( mavenProject, false /*escapedBackslashesInFilePath*/ ));
+        interpolator.addValueSource(new ValueSource() {
+			public Object getValue(String expression) {
+				return baseProps.getProperty(expression);
+			}
+        });
+
+        Properties p = new Properties();
+        
+		try {
+			InputStream is = new FileInputStream(in);
+	        try {
+	        	p.load(is);
+	        } finally {
+	        	is.close();
+	        }
+
+	        for (Map.Entry entry : p.entrySet()) {
+	        	String value = (String) entry.getValue();
+				entry.setValue(interpolator.interpolate(value));
+	        }
+	        
+	        OutputStream os = new FileOutputStream(new File(outDir, in.getName()));
+	        try {
+	        	p.store(os, null);
+	        } finally {
+	        	os.close();
+	        }
+		} catch (IOException e) {
+			throw new MojoExecutionException("Could not filter properties file", e);
+		} catch (InterpolationException e) {
+			throw new MojoExecutionException("Could not filter properties file", e);
+		}
+
+	}
+
+}
