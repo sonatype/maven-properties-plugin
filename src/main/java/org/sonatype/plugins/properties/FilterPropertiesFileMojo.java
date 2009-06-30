eabdb25387f862d9f7f5124aad0100c46119d82f
Igor Fedorenko <ifedorenko@sonatype.com>
2009-06-30 18:00:48 +0000

    updated to work with latest maven 3.0-SNAPSHOT
    git-svn-id: file:///opt/svn/repositories/sonatype.org/plugins/trunk/maven-properties-plugin@251 c2a14038-686d-4adc-8740-a14e5738cb64

--- src/main/java/org/sonatype/plugins/properties/FilterPropertiesFileMojo.java
+++ src/main/java/org/sonatype/plugins/properties/FilterPropertiesFileMojo.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -22,77 +24,105 @@ import org.codehaus.plexus.interpolation.ValueSource;
 /**
  * @goal filter-file
  */
-public class FilterPropertiesFileMojo extends AbstractMojo {
+public class FilterPropertiesFileMojo
+    extends AbstractMojo
+{
 
-	/**
-	 * @parameter expression="${project}"
-	 */
-	private MavenProject mavenProject;
-	
-	/**
-	 * @parameter expression="${session}"
-	 */
-	private MavenSession mavenSession;
-	
-	/**
-	 * @parameter
-	 * @read-only
-	 * @required
-	 */
-	private File in;
-	
-	/**
-	 * @parameter
-	 * @read-only
-	 * @required
-	 */
-	private File outDir;
+    /**
+     * @parameter expression="${project}"
+     */
+    private MavenProject mavenProject;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
+    /**
+     * @parameter expression="${session}"
+     */
+    private MavenSession mavenSession;
 
-		final Properties baseProps = new Properties();
-		if (mavenProject.getProperties() != null) {
-			baseProps.putAll(mavenProject.getProperties());
-		}
-		if (mavenSession.getExecutionProperties() != null) {
-			baseProps.putAll(mavenSession.getExecutionProperties());
-		}
-		
+    /**
+     * @parameter
+     * @read-only
+     * @required
+     */
+    private File in;
+
+    /**
+     * @parameter
+     * @read-only
+     * @required
+     */
+    private File outDir;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+
+        final Properties baseProps = new Properties();
+        if ( mavenProject.getProperties() != null )
+        {
+            baseProps.putAll( mavenProject.getProperties() );
+        }
+        if ( mavenSession.getExecutionProperties() != null )
+        {
+            baseProps.putAll( mavenSession.getExecutionProperties() );
+        }
+
         StringSearchInterpolator interpolator = new StringSearchInterpolator();
-        interpolator.addValueSource(new MavenProjectValueSource( mavenProject, false /*escapedBackslashesInFilePath*/ ));
-        interpolator.addValueSource(new ValueSource() {
-			public Object getValue(String expression) {
-				return baseProps.getProperty(expression);
-			}
-        });
+        interpolator.addValueSource( new MavenProjectValueSource( mavenProject, false /* escapedBackslashesInFilePath */) );
+        interpolator.addValueSource( new ValueSource()
+        {
+            public Object getValue( String expression )
+            {
+                return baseProps.getProperty( expression );
+            }
+
+            public void clearFeedback()
+            {
+            }
+
+            public List getFeedback()
+            {
+                return Collections.EMPTY_LIST;
+            }
+        } );
 
         Properties p = new Properties();
-        
-		try {
-			InputStream is = new FileInputStream(in);
-	        try {
-	        	p.load(is);
-	        } finally {
-	        	is.close();
-	        }
 
-	        for (Map.Entry entry : p.entrySet()) {
-	        	String value = (String) entry.getValue();
-				entry.setValue(interpolator.interpolate(value));
-	        }
-	        
-	        OutputStream os = new FileOutputStream(new File(outDir, in.getName()));
-	        try {
-	        	p.store(os, null);
-	        } finally {
-	        	os.close();
-	        }
-		} catch (IOException e) {
-			throw new MojoExecutionException("Could not filter properties file", e);
-		} catch (InterpolationException e) {
-			throw new MojoExecutionException("Could not filter properties file", e);
-		}
+        try
+        {
+            InputStream is = new FileInputStream( in );
+            try
+            {
+                p.load( is );
+            }
+            finally
+            {
+                is.close();
+            }
 
-	}
+            for ( Map.Entry entry : p.entrySet() )
+            {
+                String value = (String) entry.getValue();
+                entry.setValue( interpolator.interpolate( value ) );
+            }
 
+            OutputStream os = new FileOutputStream( new File( outDir, in.getName() ) );
+            try
+            {
+                p.store( os, null );
+            }
+            finally
+            {
+                os.close();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Could not filter properties file", e );
+        }
+        catch ( InterpolationException e )
+        {
+            throw new MojoExecutionException( "Could not filter properties file", e );
+        }
+
+    }
 }
